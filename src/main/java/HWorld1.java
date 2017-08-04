f545a169f9b2bf110447b09a912c07a30cd1eb51
xiaobian2 <2931725715@qq.com>
2017-08-04 09:06:38 +0800

    Initial commit

---
+++ src/main/java/HWorld1.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/8/4.
+ */
+public class HWorld1 {
+
+    @Test
+    public void HWorldTest() {
+        System.out.println("HelloWorld1");
+    }
+
+}
